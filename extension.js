c5d9fa2a4141c129e65dbc2ccc225cba611957f7
Alexander Fougner <fougner89@gmail.com>
2011-06-15 22:10:16 +0200

    got it working

--- extension.js
+++ extension.js
@@ -16,86 +16,68 @@ const POMODORO_CONFIG = "pomodoro-config.py";
 let _pomodoroInit = false;
 
 function Indicator() {
-	this._init.apply(this, arguments);
+    this._init.apply(this, arguments);
 }
 
 Indicator.prototype = {
-	__proto__: PanelMenu.SystemStatusButton.prototype,
+    __proto__: PanelMenu.SystemStatusButton.prototype,
 
-	_init: function() {
-		PanelMenu.SystemStatusButton.prototype._init.call(this, 'text-x-generic-symbol');
+    _init: function() {
+        PanelMenu.SystemStatusButton.prototype._init.call(this, 'text-x-generic-symbol');
 
-		this._timer = new St.Label();
-		this._timeSpent = -1;
-		this._timerActive = false;
-		this._sessionCount = 0;
+        this._timer = new St.Label();
+        this._timeSpent = -1;
+        this._stopTimer = true;
+        this._sessionCount = 0;
 
-		this._timer.set_text("[0] --:--");
-		this.actor.add_actor(this._timer);
+        this._timer.set_text("[0] --:--");
+        this.actor.add_actor(this._timer);
 
-		let tt = new PopupMenu.PopupSwitchMenuItem(_("Toggle timer"), false);
-		tt.connect("toggled", Lang.bind(this, this._toggleTimerState));
-		this.menu.addMenuItem(tt);
+        let widget = new PopupMenu.PopupSwitchMenuItem(_("Toggle timer"), false);
+        widget.connect("toggled", Lang.bind(this, this._toggleTimerState));
+        this.menu.addMenuItem(widget);
 
-		this.menu.addMenuItem(new PopupMenu.PopupSeparatorMenuItem());
+        this._refreshTimer();
+    },
 
-		let settingsitem = new PopupMenu.PopupMenuItem(_("Pomodoro Settings"));
-		settingsitem.connect('activate', function () { Util.spawn("pomodoro-config")});
-		this.menu.addMenuItem(settingsitem);
+    _toggleTimerState: function(item) {
+        this._stopTimer = item.state;
+        if (this._stopTimer == false) {
+            this._stopTimer = true;
+            this._timer.set_text("[" + this._sessionCount + "] --:--");
+        }
+        else {
+            this._timeSpent = -1;
+            this._stopTimer = false;
+            this._refreshTimer();
+        }
+    },
 
-		this._refreshTimer();
-	},
+    _refreshTimer: function() {
+        if (this._stopTimer == false) {
+            this._timeSpent += 1;
+            if (this._timeSpent > 25) {
+                this._timeSpent = 0;
+                this._sessionCount += 1;
+            }
 
-	_toggleTimerState: function(item) {
-		this._timerActive = item.state;
-		if (this._timerActive){
-			this._timeSpent = -1;
-			this._timerActive = false;
-			this._refreshTimer();
-		}
-		else {
-			this._timerActive = true;
-			this._timer.set_text("[" + this._sessionCount + "] --:--");
-		}
-	},
+            if (this._timeSpent < 10)
+                this._timer.set_text("[" + this._sessionCount + "] 00:0" + this._timeSpent.toString());
+            else
+                this._timer.set_text("[" + this._sessionCount + "] 00:" + this._timeSpent.toString());
 
-	_refreshTimer: function() {
-		if (this._timerActive) {
-			let period = Schema.get_int('period');
-			this._timeSpent += 1;
-			if (this._timeSpent > period) {
-				this._timeSpent = 0;
-				this._sessionCount += 1;
-				this._showBreak("You're on a break!", 5*60 );
-				this._toogleTimerState();
-			}
+            Mainloop.timeout_add_seconds(60, Lang.bind(this, this._refreshTimer));
+        }
 
-			if (this._timeSpent < 10)
-				this._timer.set_text("[" + this._sessionCount + "] 00:0" + this._timeSpent.toString());
-			else
-				this._timer.set_text("[" + this._sessionCount + "] 00:" + this._timeSpent.toString());
-
-			Mainloop.timeout_add_seconds(60, Lang.bind(this, this._refreshTimer));
-		}
-
-		return false;
-	},
-
-	_showBreak: function(msg, timeout) {
-		this._breakLabel = new St.Label({ style_class: 'break-label', text: msg });
-		let monitor = global.get_primary_monitor();
-		global.stage.add_actor(this._breakLabel);
-		this._breakLabel.set_position(Math.floor (monitor.width / 2 - this._breakLabel.width / 2),
-			Math.floor(monitor.height / 2 - this._breakLabel.height / 2));
-		Mainloop.timeout_add( timeout*1000, function () { this._breakLabel.destroy(); });
-	}
-}
+        return false;
+    }
+};
 
 function main() {
-	if (!_pomodoroInit) {
-		Main.StatusIconDispatcher.STANDARD_TRAY_ICON_IMPLEMENTATIONS['pomodoro'] = 'pomodoro';
-		Main.Panel.STANDARD_TRAY_ICON_ORDER.unshift('pomodoro');
-		Main.Panel.STANDARD_TRAY_ICON_SHELL_IMPLEMENTATION['pomodoro'] = Indicator;
-		_pomodoroInit = true;
-	}
+    if (!_pomodoroInit) {
+        Main.StatusIconDispatcher.STANDARD_TRAY_ICON_IMPLEMENTATIONS['pomodoro'] = 'pomodoro';
+        Main.Panel.STANDARD_TRAY_ICON_ORDER.unshift('pomodoro');
+        Main.Panel.STANDARD_TRAY_ICON_SHELL_IMPLEMENTATION['pomodoro'] = Indicator;
+        _pomodoroInit = true;
+    }
 }
